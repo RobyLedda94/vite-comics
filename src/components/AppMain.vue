04107f3a0bd0efb8844c95bf1b8d962fb9c33752
RobyLedda94 <robyledda01@yahoo.it>
2024-07-29 22:41:52 +0200

    [UPD] impostato main bottom statico, da definire il posizionamento dell'immagine

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -22,7 +22,7 @@ export default {
                 <div class='row'>
                     <div class='col-12'>
                         <div class='content-main'>
-                            <ul class='d-flex list-unstyled align-items-center justify-content-center'>
+                            <ul class='d-flex list-unstyled align-items-center justify-content-center flex-nowrap'>
                                 <li>
                                     <img src='../assets/img/buy-comics-digital-comics.png' />
                                     <span>DIGITAL COMICS</span>                                   
@@ -49,6 +49,114 @@ export default {
                 </div>
             </div>
         </div>
+        <div class='immagine-sfondo'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <div class='content-main-bottom d-flex justify-content-between py-5'>
+                            <div class='left d-flex'>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Characters
+                                    </li>
+                                    <li>
+                                        Comics
+                                    </li>
+                                    <li>
+                                        Movies
+                                    </li>
+                                    <li>
+                                        TV
+                                    </li>
+                                    <li>
+                                        Games
+                                    </li>
+                                    <li>
+                                        Videos
+                                    </li>
+                                    <li>
+                                        News
+                                    </li>
+                                    <li>
+                                        <p class='mt-3'>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Shop DC
+                                    </li>
+                                    <li>
+                                        Shop DC Collectibles
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled mx-4'>
+                                    <li>
+                                        <p>DC</p>
+                                    </li>
+                                    <li>
+                                        Terms Of Use
+                                    </li>
+                                    <li>
+                                        Privacy policy (New)
+                                    </li>
+                                    <li>
+                                        Ad Choices
+                                    </li>
+                                    <li>
+                                        Advertising
+                                    </li>
+                                    <li>
+                                        Jobs
+                                    </li>
+                                    <li>
+                                        Subscription
+                                    </li>
+                                    <li>
+                                        Talent Workshops
+                                    </li>
+                                    <li>
+                                        CPSC Certificates
+                                    </li>
+                                    <li>
+                                        Ratings
+                                    </li>
+                                    <li>
+                                        Shop Help
+                                    </li>
+                                    <li>
+                                        Contact Us
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>SITES</p>
+                                    </li>
+                                    <li>
+                                        DC
+                                    </li>
+                                    <li>
+                                        MAD Magazine
+                                    </li>
+                                    <li>
+                                        DC Kids
+                                    </li>
+                                    <li>
+                                        DC Universe
+                                    </li>
+                                    <li>
+                                        DC Power Visa
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class='right'>
+                                <img src='../assets/img/dc-logo-bg.png' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </main>
 </template>
@@ -69,11 +177,29 @@ export default {
         ul{
             li{
                 padding: 0 19px;
+                margin: 0 10px;
                 span{
-                    margin-left: 5px;
+                    margin-left: 10px;
+                    font-size: 12px;
                 }
                 img{
-                    width: 60px;
+                    width: 50px;
+                }
+            }
+        }
+    }
+    .immagine-sfondo{
+        background-image: url('../assets/img/footer-bg.jpg');
+    }
+
+    .content-main-bottom{
+        ul{
+            li{
+                font-size: 12px;
+                color: lightgrey;
+                p{
+                    color: white;
+                    font-weight: bold;
                 }
             }
         }
