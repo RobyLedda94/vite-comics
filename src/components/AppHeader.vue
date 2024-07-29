c9ffd72ee3b751b8f3f69c148184ed130665d53d
RobyLedda94 <robyledda01@yahoo.it>
2024-07-29 17:37:42 +0200

    [UPD] contenuti header, da definire lo stile

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,8 +3,24 @@
         <div class='container'>
             <div class='row'>
                 <div class='col-12'>
-                    <div class='content-header'>
-                        <img src='../assets/img/dc-logo.png' />
+                    <div class='content-header d-flex justify-content-between align-items-center py-3'>
+                        <div class='logo'>
+                            <img src='../assets/img/dc-logo.png' />
+                        </div>
+                        <div class='navigazione'>
+                            <ul class='d-flex list-unstyled'>
+                                <li>CHATACTERS</li>
+                                <li>COMICS</li>
+                                <li>MOVIES</li>
+                                <li>TV</li>
+                                <li>GAMES</li>
+                                <li>COLLECTIBLES</li>
+                                <li>VIDEOS</li>
+                                <li>FANS</li>
+                                <li>NEWS</li>
+                                <li>SHOP</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -16,6 +32,13 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+    ul {
+        li{
+            padding: 20px 20px;
+            font-size: 13px;
+            font-weight: bold;
+            color: rgb(133, 133, 133);
+        }
+    }
 </style>
